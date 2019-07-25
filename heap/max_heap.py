1558377a94f1cc12607cff571108484113058eec
sriley <sarahriley.dev@gmail.com>
2019-07-25 18:51:33 -0400

    working on sift and delete

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,33 +1,47 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    self.storage.append(value)
-    self._bubble_up(len(self.storage) -1)
+    def insert(self, value):
+        self.storage.append(value)
+        self._bubble_up(len(self.storage) - 1)
 
-  def delete(self):
-    pass
+    def delete(self):
+        max_del = self.storage[0]
+        self.storage[0] = self.storage[len(self.storage) - 1]
+        self.storage.pop()
+        self._sift_down(0)
+        return max_del
 
-  def get_max(self):
-    return self.storage[0]
+    def get_max(self):
+        return self.storage[0]
 
-  def get_size(self):
-    return len(self.size)
+    def get_size(self):
+        return len(self.storage)
 
-  def _bubble_up(self, index):
-    while index > 0:
-      #compare to parent
-      parent = (index - 1) // 2 #double divide handles the rounding
+    def _bubble_up(self, index):
+        while index > 0:
+            # compare to parent
+            parent = (index - 1) // 2  # double divide handles the rounding
 
-      if self.storage[index] > self.storage[parent]:
-        self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+            if self.storage[index] > self.storage[parent]:
+                self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
 
-        index = parent 
+                index = parent
 
-      else:
-        break
+            else:
+                break
 
-  def _sift_down(self, index):
-    
-
+    def _sift_down(self, index):
+        end = len(self.storage) - 1
+        child = index * 2 + 1
+        while child <= end:
+            right_child = child + 1
+            if right_child <= end and self.storage[index] < self.storage[right_child]:
+                child = right_child
+            if self.storage[index] < self.storage[child]:
+                self.storage[index], self.storage[child] = self.storage[child], self.storage[index]
+                index = child
+                child = index * 2 + 1
+            else:
+                break
